bbe2c630a77a389410c19c990b6b510224786f22
Rijul Tripathy <rijul.kumar1988@gmail.com>
2018-04-11 00:35:08 +0530

    Initial commit

---
+++ DogClassPlay.java
@@ -0,0 +1,24 @@
+class Dog
+{
+	int size;
+	String breed;
+	String name;
+	
+	void bark()
+	{
+		System.out.println("My name, breed and size is : " +name+" "+breed+" "+size);
+	}
+}
+
+public class DogClassPlay {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Dog mydog = new Dog();
+		mydog.size = 36;
+		mydog.breed = "Labrador";
+		mydog.name = "Laboo";
+				
+		mydog.bark();
+	}
+}
